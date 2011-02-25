6bbaeac095bed848ecec4c1ab59ca0ec01bacb20
Paddy Foran <paddy@secondbit.org>
2011-02-25 20:36:35 +0000

    Added handlers/links/add_edit.py to handling adding/editing links, as per BugzID 28 and BugzID 29

--- handlers/links/add_edit.py
+++ handlers/links/add_edit.py
@@ -1,52 +1,61 @@
-#/handlers/images/add_edit.py
+#/handlers/link/add_edit.py
 #
 #Authors:
 #   Paddy Foran <paddy@secondbit.org>
-#Last Modified: 2/22/11
+#Last Modified: 2/25/11
 #
 #Handles requests to add or edit an image.
 
 import sys, os, logging
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 
-from models.image import Image
+from models.link import Link
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class AddEditImageHandler(webapp.RequestHandler):
-    def get(self, image=None):
-        if image is None:
-            img = ""
-            shortname = ""
+class AddEditLinkHandler(webapp.RequestHandler):
+    def get(self, key=None):
+        if key is None:
+            link = Link()
+            link.name = ""
+            link.title = ""
+            link.url = ""
+            link.weight = ""
+            link.group = ""
         else:
-            image_data = Image(shortname=image)
-            image_data.get()
-            img = '<img src="/image/%s" /><br />' % image_data.shortname
-            shortname = image_data.shortname
+            link = Link(key=key)
+            link.get()
         self.response.out.write("""
-            <form enctype="multipart/form-data" method="post">
-                %s
-                <label>Image</label>
-                <input type="file" name="image" /><br />
-                <label>Short Name</label>
-                <input type="text" name="shortname" value="%s" /><br />
+            <form method="post">
+                <label>Name</label>
+                <input type="text" name="name" value="%s" /><br />
+                <label>Title</label>
+                <input type="text" name="title" value="%s" /><br />
+                <label>URL</label>
+                <input type="text" name="url" value="%s" /><br />
+                <label>Weight</label>
+                <input type="text" name="weight" value="%s" /><br />
+                <label>Group</label>
+                <input type="text" name="group" value="%s" /><br />
                 <input type="submit">
-            </form>""" % (img, shortname))
+            </form>""" % (link.name, link.title, link.url, link.weight, link.group))
 
-    def post(self, shortname=None):
-        image = Image()
-        if shortname is not None:
-            image.shortname = shortname
-            image.get()
-        image.image = self.request.POST["image"].value
-        image.mimetype = self.request.POST["image"].type
-        image.shortname = self.request.POST["shortname"]
-        image.save()
+    def post(self, key=None):
+        link = Link()
+        if key is not None:
+            link.key = key
+            link.get()
+        link.name = self.request.POST['name']
+        link.title = self.request.POST['title']
+        link.url = self.request.POST['url']
+        link.weight = self.request.POST['weight']
+        link.group = self.request.POST['group']
+        link.save()
 
 application = webapp.WSGIApplication([
-                                ('/admin/images/add', AddEditImageHandler),
-                                ('/admin/images/edit/(.*)', AddEditImageHandler),
-                                ('/admin/images/add/', AddEditImageHandler)
+                                ('/admin/links/add', AddEditLinkHandler),
+                                ('/admin/links/edit/(.*)', AddEditLinkHandler),
+                                ('/admin/links/add/', AddEditLinkHandler)
                                 ], debug=True)
 
 def main():
