e51a5a53e1726d9898e0bcc9683d76e2831c2517
Paddy Foran <paddy@secondbit.org>
2011-02-25 11:38:25 -0500

    Stubbed out handlers/images/list.py as per BugzID 65 by cloning handlers/images/view.py

---
+++ handlers/images/list.py
@@ -0,0 +1,31 @@
+#/handlers/images/view.py
+#
+#Authors:
+#   Paddy Foran <paddy@secondbit.org>
+#Last Modified: 2/22/11
+#
+#Displays an image from the datastore when called
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+
+from models.image import Image
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class ViewImageHandler(webapp.RequestHandler):
+    def get(self, shortname):
+        image = Image(shortname=shortname)
+        image.get()
+        self.response.headers['Content-Type'] = image.mimetype
+        self.response.out.write(image.image)
+
+application = webapp.WSGIApplication([
+    ('/image/(.*)', ViewImageHandler)
+], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
