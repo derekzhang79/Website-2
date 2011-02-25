f8840d4f239774255c8fbc1ba0f7e680a04ceb8e
Paddy Foran <paddy@secondbit.org>
2011-02-25 11:37:30 -0500

    Stubbed out the Link handlers (as per BugzID 28, BugzID 29, BugzID 30, BugzID 31, and BugzID 67) by cloning handlers/images

---
+++ handlers/links/add_edit.py
@@ -0,0 +1,56 @@
+#/handlers/images/add_edit.py
+#
+#Authors:
+#   Paddy Foran <paddy@secondbit.org>
+#Last Modified: 2/22/11
+#
+#Handles requests to add or edit an image.
+
+import sys, os, logging
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+
+from models.image import Image
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class AddEditImageHandler(webapp.RequestHandler):
+    def get(self, image=None):
+        if image is None:
+            img = ""
+            shortname = ""
+        else:
+            image_data = Image(shortname=image)
+            image_data.get()
+            img = '<img src="/image/%s" /><br />' % image_data.shortname
+            shortname = image_data.shortname
+        self.response.out.write("""
+            <form enctype="multipart/form-data" method="post">
+                %s
+                <label>Image</label>
+                <input type="file" name="image" /><br />
+                <label>Short Name</label>
+                <input type="text" name="shortname" value="%s" /><br />
+                <input type="submit">
+            </form>""" % (img, shortname))
+
+    def post(self, shortname=None):
+        image = Image()
+        if shortname is not None:
+            image.shortname = shortname
+            image.get()
+        image.image = self.request.POST["image"].value
+        image.mimetype = self.request.POST["image"].type
+        image.shortname = self.request.POST["shortname"]
+        image.save()
+
+application = webapp.WSGIApplication([
+                                ('/admin/images/add', AddEditImageHandler),
+                                ('/admin/images/edit/(.*)', AddEditImageHandler),
+                                ('/admin/images/add/', AddEditImageHandler)
+                                ], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
