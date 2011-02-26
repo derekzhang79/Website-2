6c7d77a7d89b6c83b067a59af2444b05ef7669e5
Paddy Foran <paddy@secondbit.org>
2011-02-25 22:47:15 -0500

    Updated /handlers/links/list.py to format Link.modified_on, and corrected template_values['title']

--- handlers/links/list.py
+++ handlers/links/list.py
@@ -28,6 +28,7 @@ class ListLinksHandler(webapp.RequestHandler):
             <th>Actions</th>
         </tr>"""
         for link in links:
+            modified_on = datetime.datetime.strftime("%m/%d/%y %H:%M")
             content += """<h2>Links</h2>
             <tr>
                 <td><a title="%s" href="%s">%s</a></td>
@@ -36,7 +37,7 @@ class ListLinksHandler(webapp.RequestHandler):
                 <td>%s</td>
                 <td>%s</td>
                 <td><a href="/admin/links/edit/%s" title="Edit %s">Edit</a> | <a href="/admin/links/reorder/%s" title="Reorder %s">Reorder</a> | <a href="/admin/links/delete/%s" title="Delete %s">Delete</a></td>
-            </tr>""" % (link.title, link.url, link.name, link.weight, link.group, link.modified_by.email(), link.modified_on, link.key(), link.name, link.group, link.group, link.key(), link.name)
+            </tr>""" % (link.title, link.url, link.name, link.weight, link.group, link.modified_by.email(), modified_on, link.key(), link.name, link.group, link.group, link.key(), link.name)
         sidebar = """<h2>Link Administration</h2>
         <p>You can edit, delete, and reorder the links in the datastore by
         clicking the appropriate link. You can also <a href="/admin/links/add"
@@ -44,7 +45,7 @@ class ListLinksHandler(webapp.RequestHandler):
         template_values = {
             'content' : content,
             'sidebar' : sidebar,
-            'title' : title
+            'title' : "Links"
         }
         path = os.path.join(os.path.dirname(__file__), "../../template/hauk", 'secondary.html')
         self.response.out.write(template.render(path, template_values))
