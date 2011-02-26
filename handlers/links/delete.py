ca21599d9da4f4b66a0950bc0eb51b387eae24cb
Paddy Foran <paddy@secondbit.org>
2011-02-25 22:28:12 -0500

    Forgot to import google.appengine.ext.webapp.template into /handlers/links/delete.py

--- handlers/links/delete.py
+++ handlers/links/delete.py
@@ -11,6 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 
 from models.link import Link
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class DeleteLinkHandler(webapp.RequestHandler):
