0787c5fd121b9903cf529a4efb9790de1a555374
Paddy Foran <paddy@secondbit.org>
2011-02-26 04:07:25 -0500

    Templated handlers/images/resize.py

--- handlers/images/resize.py
+++ handlers/images/resize.py
@@ -2,7 +2,7 @@
 #
 #Authors:
 #   Paddy Foran <paddy@secondbit.org>
-#Last Modified: 2/22/11
+#Last Modified: 2/26/11
 #
 #Displays a form to resize an image in the datastore, and calls the method 
 #to resize the image
@@ -12,21 +12,41 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 
 from models.image import Image
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class ResizeImageHandler(webapp.RequestHandler):
     def get(self, shortname):
         image = Image(shortname=shortname)
         image.get()
-        self.response.out.write("""
-            <form method="post">
-                <img src="/image/%s" /><br />
-                <label>Width</label>
-                <input type="text" value="%s" name="width" /><br />
-                <label>Height</label>
-                <input type="text" value="%s" name="height" /><br />
-                <input type="submit" />
-            </form>""" % (image.shortname, image.width, image.height))
+        content = """<h2>Resize Image "%s"</h2>
+            <p>
+                <form method="post">
+                    <img src="/image/%s" /><br />
+                    <label>Width</label>
+                    <input type="text" value="%s" name="width" /><br />
+                    <label>Height</label>
+                    <input type="text" value="%s" name="height" /><br />
+                    <input type="submit" />
+                </form>
+            </p>""" % (image.shortname, image.shortname, image.width, image.height))
+        sidebar = "<h2>Hints!</h2>
+        <p><b>Width</b>: The <i>maximum</i> width you'd like the resized image
+        to be.<br />
+        <b>Height</b>: The <i>maximum</i> height you'd like the resized image to
+        be.<br />
+        <br />
+        <i>Note: The image will preserve its aspect ratio, and not stretch or
+        crop. It will use whichever will result in a <u>smaller</u> image, of
+        the height and width. A new <u>copy</u> of the image will be created as
+        shortname_widthxheight, using the values you enter.</i></p>"""
+        template_values = {
+            'content' : content,
+            'title' : 'Resize Image "%s"' % image.shortname,
+            'sidebar' : sidebar
+        }
+        path = os.path.join(os.path.dirname(__file__), '../../template/hauk', 'secondary.html')
+        self.response.out.write(template.render(path, template_values))
 
     def post(self, shortname):
         image = Image(shortname=shortname)
