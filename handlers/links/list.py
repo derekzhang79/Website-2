c76d78c02a5794d53e60e8ca4a60757c92f685c0
Paddy Foran <paddy@secondbit.org>
2011-02-25 22:42:01 -0500

    Templated /handlers/links/list.py

--- handlers/links/list.py
+++ handlers/links/list.py
@@ -11,6 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 
 from models.link import Link
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class ListLinksHandler(webapp.RequestHandler):
@@ -19,9 +20,7 @@ class ListLinksHandler(webapp.RequestHandler):
         links = link.get_list()
         content = """<table>
         <tr>
-            <th>Name</th>
-            <th>Title</th>
-            <th>URL</th>
+            <th>Link</th>
             <th>Weight</th>
             <th>Group</th>
             <th>Modified By</th>
@@ -29,17 +28,26 @@ class ListLinksHandler(webapp.RequestHandler):
             <th>Actions</th>
         </tr>"""
         for link in links:
-            content += """<tr>
-                <td>%s</td>
-                <td>%s</td>
-                <td><a href="%s">%s</a></td>
+            content += """<h2>Links</h2>
+            <tr>
+                <td><a title="%s" href="%s">%s</a></td>
                 <td>%s</td>
                 <td>%s</td>
                 <td>%s</td>
                 <td>%s</td>
                 <td><a href="/admin/links/edit/%s" title="Edit %s">Edit</a> | <a href="/admin/links/reorder/%s" title="Reorder %s">Reorder</a> | <a href="/admin/links/delete/%s" title="Delete %s">Delete</a></td>
-            </tr>""" % (link.name, link.title, link.url, link.url, link.weight, link.group, link.modified_by.email(), link.modified_on, link.key(), link.name, link.group, link.group, link.key(), link.name)
-        self.response.out.write(content)
+            </tr>""" % (link.title, link.url, link.name, link.weight, link.group, link.modified_by.email(), link.modified_on, link.key(), link.name, link.group, link.group, link.key(), link.name)
+        sidebar = """<h2>Link Administration</h2>
+        <p>You can edit, delete, and reorder the links in the datastore by
+        clicking the appropriate link. You can also <a href="/admin/links/add"
+        title="Add a link">add a link</a> to the datastore.</p>"""
+        template_values = {
+            'content' : content,
+            'sidebar' : sidebar,
+            'title' : title
+        }
+        path = os.path.join(os.path.dirname(__file__), "../../template/hauk", 'secondary.html')
+        self.response.out.write(template.render(path, template_values))
 
 application = webapp.WSGIApplication([
     ('/admin/links', ListLinksHandler),
